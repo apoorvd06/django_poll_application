94705c3ca67ae28328b434353a2caed345b4adee
Apoorv <apoorvdeshmukh3@gmail.com>
2026-07-21 22:48:07 +0530

    fixed views.votes

--- polls/views.py
+++ polls/views.py
@@ -26,6 +26,23 @@ class ResultsView(generic.DetailView):
     template_name = "polls/results.html"
 
 
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .models import Question, Choice
+
 def vote(request, question_id):
-    # same as above, no changes needed.
-    ...
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, "polls/detail.html", {
+            "question": question,
+            "error_message": "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(
+            reverse("polls:results", args=(question.id,))
+        )
